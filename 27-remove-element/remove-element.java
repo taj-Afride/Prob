96646061273c762e94ec55fc16def3e9559cb7d8
taj-Afride <tafride.m@gmail.com>
2026-04-01 23:35:58 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (52.75%) - LeetSync

---
+++ 27-remove-element/remove-element.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int j=-1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==val){
+                j=i;
+                break;
+            }
+        }
+        if(j==-1)return nums.length;;
+        for(int i=j+1;i<nums.length;i++){
+            if(nums[i]!=val){
+                int temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                j++;
+            }
+        }
+        int k=0;
+        for(int v:nums){
+            if(v!=val){
+                k++;
+            }else{
+                break;
+            }
+        }
+        return k;
+    }
+}
