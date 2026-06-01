ab0c5a854c097bc257fb8be163a78dcfd013b428
taj-Afride <tafride.m@gmail.com>
2026-06-01 12:45:06 +0530

    Time: 1 ms (95.23%) | Memory: 43.5 MB (32.45%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+            long val = 1;
+            for (int j = 0; j <= i; j++) {
+                row.add((int) val);
+                val = val * (i - j) / (j + 1);
+            }
+            ans.add(row);
+        }
+        return ans;
+    }
+}
