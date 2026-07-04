2900f294a266eefe4991ee937babda834cb9a982
taj-Afride <tafride.m@gmail.com>
2026-07-04 18:40:49 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (46.36%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,30 +1,26 @@
 class Solution {
-    public int srt(int nums[],int l,int r,int target){
-        if(l>r){
-            return -1;
-        }
-        int mid=l+((r-l)/2);
-        if(nums[mid]==target)return mid;
-        if(nums[l]<=nums[mid]){
-            if(nums[l]<=target && nums[mid]>target){
-                return srt(nums,l,mid-1,target);
-            }
-            else{
-                 return srt(nums,mid+1,r,target);
-            }
-        }
-        else{
-            if(nums[mid]<target && nums[r]>=target){
-                return srt(nums,mid+1,r,target);
-            }
-            else{
-                 return srt(nums,l,mid-1,target);
-            }
-        }
-    }
     public int search(int[] nums, int target) {
         int l=0;
-        int r=nums.length-1;
-        return srt(nums,l,r,target);
+        int h=nums.length-1;
+        while(l<=h){
+            int mid=l+(h-l)/2;
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]>=nums[l]){
+                if(nums[l]<=target && target <=nums[mid]){
+                    h=mid-1;
+                }
+                else{
+                    l=mid+1;
+                }
+            }else{
+               if(nums[mid]<=target && target <=nums[h]){
+                    l=mid+1;
+                }
+                else{
+                    h=mid-1;
+                } 
+            }
+        }
+        return -1;   
     }
 }
