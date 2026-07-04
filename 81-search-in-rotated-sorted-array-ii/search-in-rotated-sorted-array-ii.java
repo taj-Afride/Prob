028537a18d22d4333a9e824e7883e1fd037a0015
taj-Afride <tafride.m@gmail.com>
2026-07-04 19:00:50 +0530

    Time: 0 ms (100.00%) | Memory: 45.4 MB (12.32%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,39 +1,30 @@
 class Solution {
-     public int srt(int nums[],int l,int r,int target){
-        if(l>r){
-            return -1;
-        }
-        int mid=l+((r-l)/2);
-        if(nums[mid]==target)return mid;
-        if(nums[l]==nums[mid]&& nums[mid]==nums[r]){
-            return srt(nums,l+1,r-1,target);
-        }
-        if(nums[l]<=nums[mid]){
-            if(nums[l]<=target && nums[mid]>target){
-                return srt(nums,l,mid-1,target);
-            }
-            else{
-                 return srt(nums,mid+1,r,target);
-            }
-        }
-        else{
-            if(nums[mid]<target && nums[r]>=target){
-                return srt(nums,mid+1,r,target);
-            }
-            else{
-                 return srt(nums,l,mid-1,target);
-            }
-        }
-    }
     public boolean search(int[] nums, int target) {
         int l=0;
-        int r=nums.length-1;
-        int result=srt(nums,l,r,target);
-        if(result!=-1){
-            return true;
+        int h=nums.length-1;
+        while(l<=h){
+            int mid=l+(h-l)/2;
+            if(nums[mid]==target) return true;
+            if (nums[l] == nums[mid] && nums[mid] == nums[h]) {
+                l++;
+                h--;
+            }
+            else if(nums[mid]>=nums[l]){
+                if(nums[l]<=target && nums[mid]>=target){
+                    h=mid-1;
+                }
+                else{
+                    l=mid+1;
+                }
+            }else{
+               if(nums[mid]<=target && nums[h]>=target){
+                    l=mid+1;
+                }
+                else{
+                    h=mid-1;
+                } 
+            }
         }
-        else{
-            return false;
-        }
+        return false;
     }
 }
