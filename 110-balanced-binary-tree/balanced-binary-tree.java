964275103ebc2652b2e9c13edad2a6e3e8f8b2e5
taj-Afride <tafride.m@gmail.com>
2026-07-13 10:37:38 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (95.29%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.java
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -19,19 +19,13 @@ class Solution {
            return 0;
         }
         int ls=height(root.left);
+        if(ls==-1) return -1;
         int rs=height(root.right); 
+        if(rs==-1) return -1;
+        if(Math.abs(ls-rs)>1) return -1;
         return 1+Math.max(ls,rs);
     }
     public boolean isBalanced(TreeNode root) {
-        if(root==null) return true;
-        int left=height(root.left);
-        int right=height(root.right);
-        if (Math.abs(left-right)>1) return false;
-        boolean le=isBalanced(root.left);
-        boolean r=isBalanced(root.right);
-        if(!le || !r) return false;
-        return true; 
-
-         
+       return height(root)!=-1;   
     }
 }
