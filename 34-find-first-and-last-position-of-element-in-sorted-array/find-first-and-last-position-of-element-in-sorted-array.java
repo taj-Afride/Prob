98b118124343260e053876ae31572b2974afd855
taj-Afride <tafride.m@gmail.com>
2026-08-22 19:38:56 +0530

    Time: 0 ms (100.00%) | Memory: 47.7 MB (97.00%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,44 +1,42 @@
 class Solution {
-    public int firsto(int[] nums,int n,int k){
-        int first=-1;
-        int l=0,r=n-1;
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            if(nums[mid]==k){
-                first=mid;
-                r=mid-1;
-            }
-            else if(nums[mid]<k){
+    public int findF(int nums[],int x){
+       int l=0;
+       int h=nums.length-1;
+       int ans=-1;
+       while(l<=h){
+            int mid=l+(h-l)/2;
+            if(nums[mid]>=x){
+                ans=mid;
+                h=mid-1;
+            }else{
                 l=mid+1;
             }
-            else{
-                r=mid-1;
-            }
         }
-        return first;
+        return ans; 
     }
-     public int lasto(int[] nums,int n,int k){
-        int last=-1;
-        int l=0,r=n-1;
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            if(nums[mid]==k){
-                last=mid;
-                l=mid+1;
-            }
-            else if(nums[mid]<k){
+    public int FindL(int nums[],int x){
+        int l=0;
+        int h=nums.length-1;
+        int ans=-1;
+        while(l<=h){
+            int mid=l+(h-l)/2;
+            if(nums[mid]<=x){
+                ans=mid;
                 l=mid+1;
             }
             else{
-                r=mid-1;
+                //ans=mid-1;
+                h=mid-1;
             }
         }
-        return last;
+        return ans;
     }
     public int[] searchRange(int[] nums, int target) {
-        int first=firsto(nums,nums.length,target);
-        if(first==-1) return new int[] {-1,-1};
-        int last=lasto(nums,nums.length,target);
-        return new int[] {first,last};
+        int first=findF(nums,target);
+        if(first==-1 || nums[first]!=target){
+            return new int[]{-1,-1};
+        }
+        int last=FindL(nums,target);
+        return new int[]{first,last};
     }
 }
