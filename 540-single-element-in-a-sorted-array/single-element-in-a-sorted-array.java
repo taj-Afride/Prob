f9f6b78afd38aaeca2e7b107f20b9c9bb46a1b7a
taj-Afride <tafride.m@gmail.com>
2026-08-23 14:36:35 +0530

    Time: 0 ms (100.00%) | Memory: 52.9 MB (39.88%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,32 +1,38 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int l=0;
-        int h=nums.length-1;
+        int low=0;
+        int high=nums.length-1;
         if(nums.length==1) return nums[0];
-        while(l<=h){
-            int mid=l+(h-l)/2;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            
             if (mid == 0) {
-                if (nums[0] != nums[1]) return nums[0];
+                if (nums[mid] != nums[mid + 1])
+                    return nums[mid];
             }
-            else if (mid == nums.length - 1) {
-                if (nums[mid] != nums[mid - 1]) return nums[mid];
+
+            if (mid == nums.length - 1) {
+                if (nums[mid] != nums[mid - 1])
+                    return nums[mid];
             }
-            else{
-                if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1])
+
+            if (nums[mid] != nums[mid - 1] &&
+                nums[mid] != nums[mid + 1]) {
                 return nums[mid];
             }
-            if(nums[mid]==nums[mid-1]&&nums[mid]!=nums[mid+1]){
+
+            if(nums[mid]==nums[mid-1]){
                 if(mid%2==0){
-                    h=mid-1;
-                }
-                else{
-                    l=mid+1;
+                    high=mid-1;
+                }else{
+                    low=mid+1;
                 }
             }else{
-                if(mid%2!=0){
-                    h=mid-1;
-                }else{
-                    l=mid+1;
+                if(mid%2==0){
+                    low=mid+1;
+                }
+                else{
+                    high=mid-1;
                 }
             }
         }
